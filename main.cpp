11d5a9156aaf19952d12d21b939af0987a54922f
Rikki Guy <git@rikkiguy.me.uk>
2018-12-15 12:20:36 +0000

    Split the rendering some more

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 #define RAYT_LINUX
 
 // Settings
-#define DEPTH_LIMIT 3
+#define RAYT_IMAGE_WIDTH (1000)
+#define RAYT_IMAGE_HEIGHT (500)
+#define RAYT_DEPTH_LIMIT (3)
 #define RAYT_NO_MULTISAMPLE
 #define RAYT_DETERMINISTIC
 #define RAYT_NO_OPENMP
@@ -34,7 +36,7 @@
  * - Textures
  */
 
-/*
+
 vec3 calc_colour(const ray& r, hitable* world, int depth) {
     hit_record rec;
     if(world->hit(r, 0.001, FLT_MAX, rec)) {
@@ -52,32 +54,74 @@ vec3 calc_colour(const ray& r, hitable* world, int depth) {
         return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
     }
 }
-*/
-
-
-vec3 calc_colour(const ray& r, hitable *world, int depth) {
-    hit_record rec;
-    if (world->hit(r, 0.001, FLT_MAX, rec)) { 
-        ray scattered;
-        vec3 attenuation;
-        if (depth < 50 && rec.mat->scatter(r, rec, attenuation, scattered)) {
-             return attenuation*calc_colour(scattered, world, depth+1);
-        }
-        else {
-            return vec3(0,0,0);
-        }
-    }
-    else {
-        vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
-    }
-}
 
 struct imagedata {
         int xs, ys;
         vec3* colourdata;
 };
+
+void render_to_imagedata(imagedata& img) {
+    hitable* list[4];
+    list[0] = new sphere(vec3(0,0.0,-1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    //list[0] = new sphere(vec3(0,0.0,-1), 0.5, new dielectric(1.5));
+    list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0, 0.6, 0.2), 0.3)); 
+    //list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.1));
+    list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.9, 0.9, 0.9), 0.0));
+    hitable* world = new hitable_list(list, 4);
+
+    const camera cam;
+
+
+    //vec3 imagedata[img.xs * img.ys];
+
+    #ifdef RAYT_OPENMP
+    #pragma omp parallel for
+    #endif
+    for(int j = 0; j < img.ys; ++j) {
+            #ifdef RAYT_OPENMP
+            #pragma omp parallel for
+            #endif
+            for(int i = 0; i < img.xs; ++i) {
+
+                    vec3 colour(0.0, 0.0, 0.0);
+                    
+                    #ifdef RAYT_MULTISAMPLE
+                        for(int s = 0; s < ns; ++s) {
+
+                            float u = float(i + get_rand()) / float(img.xs);
+                            float v = float(j + get_rand()) / float(img.ys);
+
+                            ray r = cam.get_ray(u, v);
+                            colour += calc_colour(r, world, 0);
+                        }
+
+                        // correct for number of samples
+                        colour /= float(ns);
+
+                    #else
+                        float u = float(i + 0.5) / float(img.xs);
+                        float v = float(j + 0.5) / float(img.ys);
+
+                        ray r = cam.get_ray(u, v);
+                        colour = calc_colour(r, world, 0); 
+                    #endif
+
+                    // Gammar correction
+                    colour = vec3( sqrt(colour[0]), sqrt(colour[1]), sqrt(colour[2]) );
+
+                    int idx = (img.ys-j-1)*img.xs + i;
+                    if(idx > img.xs*img.ys || idx < 0) {
+                        #ifdef RAYT_LINUX
+                        std::cerr << "Index out of range! " << idx << "i,j = " << i << ',' << j << '\n';
+                        #endif
+                    } else {
+                        img.colourdata[idx] = colour;
+                    }
+                    
+            }
+    }
+}
 
 
 void print_imagedata(imagedata& img) {
@@ -85,68 +129,11 @@ void print_imagedata(imagedata& img) {
         const int ns = 100;
     #endif
 
-        hitable* list[4];
-        list[0] = new sphere(vec3(0,0.0,-1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
-        //list[0] = new sphere(vec3(0,0.0,-1), 0.5, new dielectric(1.5));
-        list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-        list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0, 0.6, 0.2), 0.3)); 
-        //list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.1));
-        list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.9, 0.9, 0.9), 0.0));
-        hitable* world = new hitable_list(list, 4);
-
-        const camera cam;
-
-
-        vec3 imagedata[img.xs * img.ys];
-
-        #ifdef RAYT_OPENMP
-        #pragma omp parallel for
-        #endif
-        for(int j = 0; j < img.ys; ++j) {
-                #ifdef RAYT_OPENMP
-                #pragma omp parallel for
-                #endif
-                for(int i = 0; i < img.xs; ++i) {
-
-                        vec3 colour(0.0, 0.0, 0.0);
-                       
-                        #ifdef RAYT_MULTISAMPLE
-                            for(int s = 0; s < ns; ++s) {
-
-                                float u = float(i + get_rand()) / float(img.xs);
-                                float v = float(j + get_rand()) / float(img.ys);
-
-                                ray r = cam.get_ray(u, v);
-                                colour += calc_colour(r, world, 0);
-                            }
-
-                            // correct for number of samples
-                            colour /= float(ns);
-
-                        #else
-                            float u = float(i + 0.5) / float(img.xs);
-                            float v = float(j + 0.5) / float(img.ys);
-
-                            ray r = cam.get_ray(u, v);
-                            colour = calc_colour(r, world, 0); 
-                        #endif
-
-                        // Gammar correction
-                        colour = vec3( sqrt(colour[0]), sqrt(colour[1]), sqrt(colour[2]) );
-
-                        int idx = (img.ys-j-1)*img.xs + i;
-                        if(idx > img.xs*img.ys || idx < 0) {
-                            std::cerr << "Index out of range! " << idx << "i,j = " << i << ',' << j << '\n';
-                        } else {
-                            imagedata[idx] = colour;
-                        }
-                        
-                }
-        }
+        
 
         std::cout << "P3\n" << img.xs << ' ' << img.ys << "\n255\n";
         for(int p = 0; p < img.xs*img.ys; ++p) {
-            vec3 colour = imagedata[p];
+            vec3 colour = img.colourdata[p];
 
             // convert colour to integers for output
             int ir = int(255.99 * colour.r());
@@ -157,9 +144,12 @@ void print_imagedata(imagedata& img) {
         }
 }
 
+vec3 colourdata[RAYT_IMAGE_WIDTH * RAYT_IMAGE_HEIGHT];
+imagedata image = { RAYT_IMAGE_WIDTH, RAYT_IMAGE_HEIGHT, &colourdata[0] };
+
 #ifdef RAYT_LINUX
 int main() {
-        imagedata image = { 1000, 500 };
-        print_imagedata(image);
+    render_to_imagedata(image);
+    print_imagedata(image);
 }
 #endif
